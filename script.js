338f1339f0446875e010da72295c844edf0086cd
ali85k <aliceanndraad56@gmail.com>
2022-07-01 10:41:06 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,40 @@
+letsobreMim={ 
+    nome:"Alice", 
+    idade:19, 
+    altura:1.70
+    } 
+    console.log(sobreMim); 
+   
+   
+    sobreMim.corDosOlhos="Castanhosclaro"; 
+    console.log(sobreMim); 
+   
+   
+    letcadastro=[{ 
+    nome:"Julia", 
+    idade:26, telefone:875960700, 
+    amigos:["Dandara","Livia","Mari","Anderson"] 
+    },{ 
+    nome:"Dandara", 
+    idade:19, 
+    telefone:875960700, 
+    amigos:["Lenadro","Adriana","Luan","Romulo"] 
+    },{ 
+    nome:"Mariana", 
+    idade:27, 
+    telefone: 875960700, 
+    amigos:["Julia","Paulo", Gustavo","Anaiza"] 
+    },{ 
+    nome:"Katia", 
+    idade:28, 
+    telefone:875960700, 
+    amigos:["Daliany","Gabriel","Vitória","Julia"] 
+    },{ 
+    nome:"Paula", 
+    idade:28, 
+    telefone:875960700, 
+    amigos:["Paula","Adriana","Bruno","Amanda","Paula"] 
+    }]  
+    for(letcontador=0;contador<cadastro.length;contaador++){ 
+    console.log(cadastro[contador].amigos[contador]); 
+    }
